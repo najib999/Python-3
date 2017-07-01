c09c0a6dd39e6f5782d3fba7ff701fa243c68471
najib999 <najib999@gmail.com>
2017-07-01 00:31:20 -0500

    Add files via upload

---
+++ untitled38.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 30 20:55:09 2017
+
+@author: Shahriar Kabir
+"""
+
+def flatten(aList):
+    ls = []
+    def interior(aList):
+        for i in aList:
+            if type(i) == list:
+                interior(i)
+            else:
+                ls.append(i)
+        return ls
+    return interior(aList)
+
+print(flatten([[1,'a',['cat'],2],[[[3]],'dog'],4,5]))
