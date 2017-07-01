909b5ce7e75e211936e71be3ea4ecb2c3a764b79
najib999 <najib999@gmail.com>
2017-07-01 00:23:53 -0500

    Update ps3_hangman.py

--- ps3_hangman.py
+++ ps3_hangman.py
@@ -1,11 +1,6 @@
 # Hangman game
-#
 
 # -----------------------------------
-# Helper code
-# You don't need to understand this helper code,
-# but you will have to know how to use the functions
-# (so be sure to read the docstrings!)
 
 import random
 import string
@@ -51,7 +46,6 @@ def isWordGuessed(secretWord, lettersGuessed):
     returns: boolean, True if all the letters of secretWord are in lettersGuessed;
       False otherwise
     '''
-    # FILL IN YOUR CODE HERE...
     count = 0
     for i in secretWord:
         if i in lettersGuessed:
@@ -69,7 +63,6 @@ def getGuessedWord(secretWord, lettersGuessed):
     returns: string, comprised of letters and underscores that represents
       what letters in secretWord have been guessed so far.
     '''
-    # FILL IN YOUR CODE HERE...
     aStr = ''
     for i in secretWord:
         if i in lettersGuessed:
@@ -85,7 +78,6 @@ def getAvailableLetters(lettersGuessed):
     returns: string, comprised of letters that represents what letters have not
       yet been guessed.
     '''
-    # FILL IN YOUR CODE HERE...
     aStr = ''
     for i in string.ascii_lowercase:
         if not i in lettersGuessed:
@@ -112,7 +104,6 @@ def hangman(secretWord):
 
     Follows the other limitations detailed in the problem write-up.
     '''
-    # FILL IN YOUR CODE HERE...
     print("Welcome to the game, Hangman!")
     if len(secretWord) > 1:
         print("I'm thinking of a word that is",len(secretWord),"letters long.")
@@ -145,10 +136,6 @@ def hangman(secretWord):
         print("Congratulations, you won!")
     else:
         print("Sorry, you ran out of guesses. The word was",secretWord+'.')
-
-# When you've completed your hangman function, uncomment these two lines
-# and run this file to test! (hint: you might want to pick your own
-# secretWord while you're testing)
 
 secretWord = chooseWord(wordlist)
 hangman(secretWord)
