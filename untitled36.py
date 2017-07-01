499d975b50ed9412862697968a2f64a73c210ad1
najib999 <najib999@gmail.com>
2017-07-01 00:40:43 -0500

    Update untitled36.py

--- untitled36.py
+++ untitled36.py
@@ -9,8 +9,7 @@ def deep_reverse(L):
     """ 
     assumes L is a list of lists whose elements are ints
     Mutates L such that it reverses its elements and also 
-    reverses the order of the int elements in every element of L. 
-    It does not return anything.
+    reverses the order of the int elements in every element of L.
     """
     aList = []
     for i in L[-1::-1]:
